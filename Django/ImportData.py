7d932cfb67705eda9b9bf4fed5257a0b343c9fd5
Hephyrius <Hephyrius@gmail.com>
2018-04-20 18:14:49 +0100

    Initial Data Creation Stage
    added scripts to get data from etherscan and then process it.
    created file structures

---
+++ Django/ImportData.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 20 16:05:30 2018
+
+@author: Khera
+"""
+#library imports
+import numpy as np
+import pandas as pd
+import requests
+import json
+
+#api ley for etherscan
+api_key = "3YT45XBZMKGYDF8IDECEXN8V4M9FX59MH7"
+
+#loopringimpl deployment address
+loopringimpl_address = "0x4019440BBe5882915db9F2cAe972a518DB346C62"
+
+#api endpoint and parameters for requesting of data
+endpoint = "https://api.etherscan.io/api?module=logs&action=getLogs"
+from_block = "0"
+to_block = "latest"
+
+#constructing the api reqruest
+request = endpoint + "&fromBlock=" + from_block + "&toBlock=" + to_block + "&address=" + loopringimpl_address + "&apikey="+api_key
+
+#getting the data from the server
+data = requests.get(request).json()['result']
+
+#process data and find instances where the topic is the ring mined topic
+columns = ["data","dataset","ringsize","rawdata", "transactionhash"]
+stored_data = pd.DataFrame(columns=columns)
+topic = "0x36fe6e0e52b9db0206978557b0c50ef564aeb27c4fce2455a2f459aeefefc23b" 
+data2 = []
+for i in data:
+    if topic in i['topics']:
+        data2.append(i)
+
+#iterate over the data and extract the relevant parts, processing accordingly
+for i in data2:
+    ContractOutput = []
+    for j in range(2,len(i['data']),64):
+        ContractOutput.append(i['data'][j:j+64])
+        
+    ringsize = len(ContractOutput) - 7
+    if ringsize % 2 == 0:
+        ringsize = 2
+    elif ringsize % 3 == 0:
+        ringsize = 3
+    else:
+        ringsize = 0
+    
+    orderHashlist = []
+    for j in range(ringsize):
+        orderHashlist.append(ContractOutput[j+6])
+    intList = []
+    for j in range((ringsize*6)):
+        uint = ContractOutput[j+ringsize+6+1]
+        uint = int(uint, 16)
+        intList.append(uint)
+            
+    dataSet = {'ringIndex':int(ContractOutput[0], 16), 'ringHash':ContractOutput[1], 'miner':ContractOutput[3],'feeRecipient':ContractOutput[4],'orderHashlist':orderHashlist,'intList':intList}
+    
+    result=pd.Series([ContractOutput, dataSet, ringsize, i['data'],i['transactionHash']], index=columns)
+    stored_data = stored_data.append(result, ignore_index=True)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#topic = "0x36fe6e0e52b9db0206978557b0c50ef564aeb27c4fce2455a2f459aeefefc23b"
+#topic1 = "0x80af788bd76e7a6ea808e34ec8ad3feb8f1d044df699e01824c9a64ca7c2742c"
+#topicRequest = request = endpoint + "&fromBlock=" + from_block + "&toBlock=" + to_block + "&address=" + loopringimpl_address + "&topic=" + topic1 + "&apikey="+api_key
+#
+#topicData = requests.get(topicRequest).json()['result']

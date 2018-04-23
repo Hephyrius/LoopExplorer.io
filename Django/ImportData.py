4775c82482355563172281411047a65dc90381a8
Hephyrius <Hephyrius@gmail.com>
2018-04-23 04:04:15 +0100

    Minor DB updates
    updated DB script
    updated script to work with 1.5 of protocol
    changed ringpage so that it has two versions. Basic is prototype version as less complex, advanced is meant for beyond prototype
    Changed the CSS based theme.

--- Django/ImportData.py
+++ Django/ImportData.py
@@ -17,7 +17,7 @@ import datetime as DT
 api_key = "3YT45XBZMKGYDF8IDECEXN8V4M9FX59MH7"
 
 #loopringimpl deployment address
-loopringimpl_address = "0x4019440BBe5882915db9F2cAe972a518DB346C62"
+loopringimpl_address = "0xb1170dE31c7f72aB62535862C97F5209E356991b"
 
 #api endpoint and parameters for requesting of data
 endpoint = "https://api.etherscan.io/api?module=logs&action=getLogs"
@@ -33,7 +33,7 @@ data = requests.get(request).json()['result']
 #process data and find instances where the topic is the ring mined topic
 columns = ["data","dataset","ringsize","rawdata", "transactionhash", 'block', 'timestamp']
 stored_data = pd.DataFrame(columns=columns)
-topic = "0x36fe6e0e52b9db0206978557b0c50ef564aeb27c4fce2455a2f459aeefefc23b" 
+topic = "0x5b7c7d78703a122ea2c422b2e7bdc79c5da9fc6987bd505b3bef9782c457fd64" 
 data2 = []
 for i in data:
     if topic in i['topics']:
@@ -45,27 +45,27 @@ for i in data2:
     for j in range(2,len(i['data']),64):
         ContractOutput.append(i['data'][j:j+64])
         
-    ringsize = len(ContractOutput) - 7
-    if ringsize % 2 == 0:
+    ringsize = len(ContractOutput) - 6
+    if ringsize / 2 == 7:
         ringsize = 2
-    elif ringsize % 3 == 0:
+    elif ringsize / 3 == 7:
         ringsize = 3
     else:
         ringsize = 0
     
     orderHashlist = []
     for j in range(ringsize):
-        orderHashlist.append(ContractOutput[j+6])
+        orderHashlist.append(ContractOutput[j+5])
     intList = []
     for j in range((ringsize*6)):
-        uint = ContractOutput[j+ringsize+6+1]
+        uint = ContractOutput[j+ringsize+5+1]
+        print(j+ringsize+5)
         uint = int(uint, 16)
         intList.append(uint)
     
     ringhash = ContractOutput[1][24:]
-    miner = "0x"+ContractOutput[3][24:]
-    feerecp = "0x"+ContractOutput[4][24:]
-    dataSet = {'ringIndex':int(ContractOutput[0], 16), 'ringHash':ringhash, 'miner':miner,'feeRecipient':feerecp,'orderHashlist':orderHashlist,'intList':intList}
+    miner = "0x"+ContractOutput[2][24:]
+    dataSet = {'ringIndex':int(ContractOutput[0], 16), 'ringHash':ringhash, 'miner':miner,'orderHashlist':orderHashlist,'intList':intList}
     block = int(i['blockNumber'], 16)
     timestamp = 0 # TODO
     result=pd.Series([ContractOutput, dataSet, ringsize, i['data'],i['transactionHash'], block, timestamp], index=columns)
@@ -73,19 +73,27 @@ for i in data2:
     
 #%%
     
-##create a sqlite file
-#conn = sqlite3.connect(r"Data.db")
-#c = conn.cursor()
-#c.execute("""CREATE TABLE ringdata
-#                 (ringsize, ringindex, block, timestamp, chain, ringhash, mineraddress, minerlrc, minerx, minery, minerz, address01, traded01, market01, amount01, fill01, lrcfee01, orderhash01, feepaid01, address02, traded02, market02, amount02, fill02, lrcfee02, orderhash02, feepaid02, address03, traded03, market03, amount03, fill03, lrcfee03, orderhash03, feepaid03)""")
-#%%
+#create a sqlite file
+conn = sqlite3.connect(r"Data.db")
+c = conn.cursor()
+c.execute("""CREATE TABLE ringdata
+                 (ringsize, ringindex, block, timestamp, chain, ringhash, mineraddress, order1hash, order1lrcReward, order1lrcFeeState, order1splitS, order1splitB, order2hash, order2lrcReward, order2lrcFeeState, order2splitS, order2splitB, order3hash, order3lrcReward, order3lrcFeeState, order3splitS, order3splitB)""")
+
+
 #insert the data into the db
 for i, row in stored_data.iterrows():
-    command = "INSERT INTO ringdata (ringsize, ringindex, block, timestamp, chain, ringhash, mineraddress, minerlrc, minerx, minery, minerz, address01, traded01, market01, amount01, fill01, lrcfee01, orderhash01, feepaid01, address02, traded02, market02, amount02, fill02, lrcfee02, orderhash02, feepaid02, address03, traded03, market03, amount03, fill03, lrcfee03, orderhash03, feepaid03)"
-    command = command + " \nvalues(" + str(row['ringsize']) +", " + str(row['dataset']['ringIndex']) +", "+ str(row['block']) +", "+ str(row['timestamp']) +", Ethereum, " + str(row['dataset']['ringHash']) +", " + str(row['dataset']['miner']) +", "
+    command = "INSERT INTO ringdata (ringsize, ringindex, block, timestamp, chain, ringhash, mineraddress, order1hash, order1lrcReward, order1lrcFeeState, order1splitS, order1splitB, order2hash, order2lrcReward, order2lrcFeeState, order2splitS, order2splitB, order3hash, order3lrcReward, order3lrcFeeState, order3splitS, order3splitB)"
+    command = command + "values(" + str(row['ringsize']) +", " + str(row['dataset']['ringIndex']) +", "+ str(row['block']) +", "+ str(row['timestamp']) +", `Ethereum`, `" + row['dataset']['ringHash'] +"`, `" + row['dataset']['miner'] +"`, "
+    command = command + "`" + str(row['dataset']['orderHashlist'][0]) + "`, " + str(row['dataset']['intList'][2]) + ", " + str(row['dataset']['intList'][3]) + ", " + str(row['dataset']['intList'][4]) + ", " + str(row['dataset']['intList'][5]) + ", "
+    command = command + "`" + str(row['dataset']['orderHashlist'][1]) + "`, " + str(row['dataset']['intList'][8]) + ", " + str(row['dataset']['intList'][9]) + ", " + str(row['dataset']['intList'][10]) + ", " + str(row['dataset']['intList'][11]) + ", "
+    if(row['ringsize'] == 3):
+        command = command + "`" + str(row['dataset']['orderHashlist'][2]) + "`, " + str(row['dataset']['intList'][14]) + ", " + str(row['dataset']['intList'][15]) + ", " + str(row['dataset']['intList'][16]) + ", " + str(row['dataset']['intList'][17]) + ")"
+    else:
+        command = command + str(0) + ", " + str(0) + ", " + str(0) + ", " + str(0) + ", " + str(0) +")"
     print(command)
-    #c.execute(command)
-#conn.close()
+    c.execute(command)
+
+conn.close()
 
 #%%
 

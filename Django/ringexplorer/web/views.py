54e04111157328ecbeae06c91c28e350dde8f23e
Hephyrius <Hephyrius@gmail.com>
2018-04-23 05:19:03 +0100

    Started Wiring up frontend with Django backend

--- Django/ringexplorer/web/views.py
+++ Django/ringexplorer/web/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template.context import RequestContext
 
-# Create your views here.
+def index(request):
+	return render(request, 'HomePage.html')
+	
+def allRings(request):
+	return render(request, 'AllRingsPage.html')
+	
+def getRing(request):
+	return render(request, 'RingBasic.html')

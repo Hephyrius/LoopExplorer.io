8e8369dfcc2b09c05306a9fc07a033a1946743d6
Hephyrius <Hephyrius@gmail.com>
2018-04-23 12:53:30 +0100

    Begin Linking view to model
    Added link from home -> all rings
    Added loading table from database for all rings

---
+++ Django/ringexplorer/web/forms.py
@@ -0,0 +1,2 @@
+from django.forms import ModelForm, TextInput
+from .models import ring
--- Django/ringexplorer/web/views.py
+++ Django/ringexplorer/web/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, render_to_response
 from django.template.context import RequestContext
+from .models import ring
 
 def index(request):
 	return render(request, 'HomePage.html')
 	
 def allRings(request):
-	return render(request, 'AllRingsPage.html')
+	data = ring.objects.all()
+	print(data)
+	#return render(request, 'AllRingsPage.html')
+	return render_to_response('AllRingsPage.html', {'result':data})
 	
 def getRing(request):
 	return render(request, 'RingBasic.html')

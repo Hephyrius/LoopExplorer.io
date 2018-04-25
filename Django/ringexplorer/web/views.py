82bb3cd4214a8e4a79d246b8272f7d348cf70ad4
Hephyrius <Hephyrius@gmail.com>
2018-04-25 11:44:03 +0100

    Fixed Logics
    Rate Logic calculation changed
    Changed when order3 should be shown

--- Django/ringexplorer/web/views.py
+++ Django/ringexplorer/web/views.py
@@ -26,7 +26,7 @@ def allRings(request):
 	
 def getRing(request):
 	query = request.GET.get('ringindex')
-	data = ring.objects.filter(ringindex = query).annotate(rate1=F('order1amount') / F('order1nextamount')).annotate(rate2=F('order2amount') / F('order2nextamount')).annotate(rate3=F('order3amount') / F('order3nextamount'))
+	data = ring.objects.filter(ringindex = query).annotate(rate1=F('order1nextamount')/F('order1amount')).annotate(rate2=F('order2nextamount')/F('order2amount')).annotate(rate3=F('order3nextamount')/F('order3amount'))
 
 	form = ringIndexForm()
 	context={'result':data[0],'form': form}
